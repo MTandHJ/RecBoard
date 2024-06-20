0ecca80e90016880080806805eeb9efc7411e0d3
MTandHJ <congxueric@gmail.com>
2024-06-20 13:25:45 +0800

    add GCN

---
+++ GCN/main.py
@@ -0,0 +1,179 @@
+
+
+from typing import Dict, Tuple
+
+import torch
+import torch.nn as nn
+import freerec
+
+freerec.declare(version='1.0.1')
+
+cfg = freerec.parser.Parser()
+cfg.add_argument("--embedding-dim", type=int, default=64)
+cfg.add_argument("--num-layers", type=int, default=3)
+
+cfg.set_defaults(
+    description="GCN",
+    root="../../data",
+    dataset='Yelp2018_10104811_ROU',
+    epochs=1000,
+    batch_size=2048,
+    optimizer='adam',
+    lr=1e-3,
+    weight_decay=1e-4,
+    seed=1
+)
+cfg.compile()
+
+
+class GCN(freerec.models.GenRecArch):
+
+    def __init__(
+        self, dataset: freerec.data.datasets.RecDataSet,
+        embedding_dim: int = 64, num_layers: int = 3
+    ) -> None:
+        super().__init__(dataset)
+
+        self.num_layers = num_layers
+
+        self.User.add_module(
+            "embeddings", nn.Embedding(
+                self.User.count, embedding_dim
+            )
+        )
+
+        self.Item.add_module(
+            "embeddings", nn.Embedding(
+                self.Item.count, embedding_dim
+            )
+        )
+
+        self.register_buffer(
+            "Adj",
+            self.dataset.train().to_normalized_adj(
+                normalization='sym'
+            )
+        )
+
+        self.act = nn.ReLU()
+        self.linears = nn.ModuleList([
+            nn.Linear(embedding_dim, embedding_dim) for _ in range(self.num_layers)
+        ])
+
+        self.criterion = freerec.criterions.BPRLoss(reduction='mean')
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.)
+            elif isinstance(m, nn.Embedding):
+                nn.init.normal_(m.weight, std=1.e-4)
+            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+                nn.init.constant_(m.weight, 1.)
+                nn.init.constant_(m.bias, 0.)
+
+    def sure_trainpipe(self, batch_size: int):
+        return self.dataset.train().choiced_user_ids_source(
+        ).gen_train_sampling_pos_().gen_train_sampling_neg_(
+            num_negatives=1
+        ).batch_(batch_size).tensor_()
+
+    def encode(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        allEmbds = torch.cat(
+            (self.User.embeddings.weight, self.Item.embeddings.weight), dim=0
+        ) # (N, D)
+        for l in range(self.num_layers - 1):
+            allEmbds = self.Adj @ allEmbds
+            allEmbds = self.act(
+                self.linears[l](allEmbds)
+            )
+        allEmbds = self.Adj @ allEmbds
+        allEmbds = self.linears[-1](allEmbds)
+        userEmbds, itemEmbds = torch.split(
+            allEmbds, (self.User.count, self.Item.count)
+        )
+        return userEmbds, itemEmbds
+
+    def fit(self, data: Dict[freerec.data.fields.Field, torch.Tensor]):
+        userEmbds, itemEmbds = self.encode()
+        users, positives, negatives = data[self.User], data[self.IPos], data[self.INeg]
+        userEmbds = userEmbds[users] # (B, 1, D)
+        iposEmbds = itemEmbds[positives] # (B, 1, D)
+        inegEmbds = itemEmbds[negatives] # (B, K, D)
+
+        rec_loss = self.criterion(
+            torch.einsum("BKD,BKD->BK", userEmbds, iposEmbds),
+            torch.einsum("BKD,BKD->BK", userEmbds, inegEmbds)
+        )
+        return rec_loss
+
+    def reset_ranking_buffers(self):
+        """This method will be executed before evaluation."""
+        userEmbds, itemEmbds = self.encode()
+        self.ranking_buffer = dict()
+        self.ranking_buffer[self.User] = userEmbds.detach().clone()
+        self.ranking_buffer[self.Item] = itemEmbds.detach().clone()
+
+    def recommend_from_full(self, data: Dict[freerec.data.fields.Field, torch.Tensor]):
+        userEmbds = self.ranking_buffer[self.User][data[self.User]] # (B, 1, D)
+        itemEmbds = self.ranking_buffer[self.Item]
+        return torch.einsum("BKD,ND->BN", userEmbds, itemEmbds)
+
+    def recommend_from_pool(self, data: Dict[freerec.data.fields.Field, torch.Tensor]):
+        userEmbds = self.ranking_buffer[self.User][data[self.User]] # (B, 1, D)
+        itemEmbds = self.ranking_buffer[self.Item][data[self.IUnseen]] # (B, 101, D)
+        return torch.einsum("BKD,BKD->BK", userEmbds, itemEmbds)
+
+
+class CoachForGCN(freerec.launcher.Coach):
+
+    def train_per_epoch(self, epoch: int):
+        for data in self.dataloader:
+            data = self.dict_to_device(data)
+            rec_loss = self.model(data)
+            loss = rec_loss
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+            self.monitor(
+                loss.item(), 
+                n=len(data[self.User]), reduction="mean", 
+                mode='train', pool=['LOSS']
+            )
+
+
+def main():
+
+    try:
+        dataset = getattr(freerec.data.datasets, cfg.dataset)(root=cfg.root)
+    except AttributeError:
+        dataset = freerec.data.datasets.RecDataSet(cfg.root, cfg.dataset, tasktag=cfg.tasktag)
+
+    model = GCN(
+        dataset,
+        embedding_dim=cfg.embedding_dim, num_layers=cfg.num_layers
+    )
+
+    trainpipe = model.sure_trainpipe(cfg.batch_size)
+    validpipe = model.sure_validpipe(cfg.ranking)
+    testpipe = model.sure_testpipe(cfg.ranking)
+
+    coach = CoachForGCN(
+        dataset=dataset,
+        trainpipe=trainpipe,
+        validpipe=validpipe,
+        testpipe=testpipe,
+        model=model,
+        cfg=cfg
+    )
+    coach.fit()
+
+
+if __name__ == "__main__":
+    main()
